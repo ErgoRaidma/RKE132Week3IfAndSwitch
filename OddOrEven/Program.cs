88ed9c44f6047ddb3ee70dfdc68078ef8b71f990
Ergo <ergo.raidma@tktk.ee>
2024-02-04 12:46:52 +0200

    add week3 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Console.WriteLine("Enter a number");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+//kas kasutaja arv on paaris või paaritu
+
+int resault = userNum % 2;
+
+if(resault != 0)//!= tähendab, et ei ole võrdne
+{
+    Console.WriteLine("User number is odd.");
+}else
+{
+    Console.WriteLine("User number is even");
+}
